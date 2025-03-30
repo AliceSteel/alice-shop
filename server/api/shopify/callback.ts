b3a2db34c59ebe7c1e638acd9c2d2c4f95ca39a7
Olga Garkusha <olga.garkusha.uk@gmail.com>
2025-03-30 20:25:26 +0200

    updated login 9

--- server/api/shopify/callback.ts
+++ server/api/shopify/callback.ts
@@ -3,23 +3,29 @@ import { defineEventHandler, readBody } from 'h3'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const tokenRequestUrl = 'https://shopify.com/authentication/62268506202/oauth/token'
+  let params = new URLSearchParams({
+    grant_type: body.grant_type,
+    client_id: body.client_id,
+  })
+
+  if (body.grant_type === 'authorization_code') {
+    params.append('code', body.code)
+    params.append('redirect_uri', body.redirect_uri)
+    params.append('code_verifier', body.code_verifier)
+  } else if (body.grant_type === 'refresh_token') {
+    params.append('refresh_token', body.refresh_token)
+  }
 
   const req = await fetch(tokenRequestUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: new URLSearchParams({
-      grant_type: body.grant_type,
-      client_id: body.client_id,
-      code: body.code,
-      redirect_uri: body.redirect_uri,
-      code_verifier: body.code_verifier,
-      refresh_token: body.refresh_token // for refresh case
-    })
+    body: params
   })
 
   if (!req.ok) {
+    console.log('response ids not ok', req)
     return { error: true, status: req.status }
   }
 
